84322f880a2bf89c3616596b315d523a79b421ea
ntrijullo <enzot.13@gmail.com>
2023-05-16 11:28:04 -0400

    creacion ruta de usuarios

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const user = require('./routes/users')
 
 app.get('/',(req,res)=>{
     res.send('Bienvenidos');
 });
 
-app.get('/users',(req,res)=>{
-    res.send('mostrando usuarios');
-});
+app.use(user);
 
 app.listen(port,()=>{
     console.log('Server por puerto 3000');
